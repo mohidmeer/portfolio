a655ad2a97b1ccb3f6f94d2d281fc230094a4397
Mohid Meer <mohidfauji@gmail.com>
2026-06-01 06:41:58 +0500

    updated

--- app/(valorant)/val/page.tsx
+++ app/(valorant)/val/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Settings } from 'lucide-react'
 import Image from 'next/image'
 import { assetUrl } from "@/lib/asset-url";
